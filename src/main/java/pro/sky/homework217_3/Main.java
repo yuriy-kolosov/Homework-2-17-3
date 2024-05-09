2ae49c7a23a830c0c7327a340a346a850bbe771c
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-09 23:23:38 +0300

    Домашнее задание 2-17. Основы алгоритмов. Часть 3 - начальный коммит

---
+++ src/main/java/pro/sky/homework217_3/Main.java
@@ -0,0 +1,9 @@
+package pro.sky.homework217_3;
+
+public class Main {
+    public static void main(String[] args) {
+        //                                              Домашнее задание 2-17. Основы алгоритмов. Часть 3
+        System.out.println("Домашнее задание 2-17. Основы алгоритмов. Часть 3");
+        System.out.println();
+    }
+}
